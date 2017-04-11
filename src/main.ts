55d549022d351e1c2de985e201d69f76091bc29d
Matt Gadd <mgadd@sitepen.com>
2017-04-11 17:15:53 +0100

    add path to browser runner

--- src/main.ts
+++ src/main.ts
@@ -135,6 +135,7 @@ const command: Command = {
 				() => {
 					runTests(args)
 						.then(() => {
+							console.log('\n to run in browser: ' + underline('./node_modules/intern/client.html?config=node_modules/@dojo/cli-test-intern/intern/intern'));
 							process.removeListener('unhandledRejection', unhandledRejection);
 						})
 						.then(resolve, reject);
